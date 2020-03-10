03695b2ab01f883f7a7a0752b0d59a896a295ea8
Upendra Parsad <upendra@Upendras-MacBook-Pro.local>
2020-03-10 01:22:25 -0300

    Created the basic structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
+
 export class AppComponent {
-  title = 'web-app';
+  title = "web-app";
+  form: FormGroup;
+  constructor(fb: FormBuilder) {
+    this.form = fb.group({
+      search: ["", Validators.required]
+    });
+  }
+
+  onClickSubmit() {
+    
+  }
 }
